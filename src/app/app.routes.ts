893c7f3acfdec0de1ce4b0041ab2045aec0ece01
vieerr <olivier_paspuel@hotmail.com>
2025-12-21 17:58:06 -0500

    feat: 🎸 add app files, with routing & navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'pos',
+        pathMatch: 'full'
+    },
+    {
+        path: 'pos',
+        loadComponent: () => import('./pos-terminal/pos-terminal').then(m => m.PosTerminalComponent)
+    },
+    {
+        path: 'inventory',
+        loadComponent: () => import('./inventory/inventory').then(m => m.InventoryComponent)
+    }
+];
