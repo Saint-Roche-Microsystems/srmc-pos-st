2aa5eea47cdbb0f3b3b8db256f079b665e38b223
vieerr <olivier_paspuel@hotmail.com>
2025-12-21 21:59:45 -0500

    test: 💍 add basic tests for app

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideAnimationsAsync()
+      ]
     }).compileComponents();
   });
 
@@ -14,10 +22,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render navbar with brand name', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, srmc-pos-st');
+    expect(compiled.querySelector('span')?.textContent).toContain('SRMC-POS');
   });
 });
