a7819ebecac42ffe452aec88024d44df7b50e491
vieerr <olivier_paspuel@hotmail.com>
2026-02-11 23:28:25 -0500

    ci: 🎡 integrate new ec2 service WIP

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:3000/api' // Change this to your actual backend URL
+    baseUrl: 'http://54.235.61.45:3000/api' // Change this to your actual backend URL
 };
