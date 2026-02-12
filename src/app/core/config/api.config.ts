620791abe8d15abd059f6e71b4ca4b59acc131a4
vieerr <olivier_paspuel@hotmail.com>
2026-02-12 08:18:44 -0500

    ci: 🎡 add new api url to pj

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    baseUrl: 'http://54.235.61.45:3000/api' // Change this to your actual backend URL
+    baseUrl: 'https://srmc-pos.vieerr.xyz/api' // Change this to your actual backend URL
 };
